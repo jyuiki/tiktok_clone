23cf4df7239572096fa8bfeb5b3ab39a29add4db
daeheonKim <jyuiki@gmail.com>
2023-06-14 16:39:20 +0900

    4.9 Login Form

--- lib/features/authentication/widgets/form_button.dart
+++ lib/features/authentication/widgets/form_button.dart
@@ -5,9 +5,11 @@ class FormButton extends StatelessWidget {
   const FormButton({
     super.key,
     required this.disabled,
+    this.text,
   });
 
   final bool disabled;
+  final String? text;
 
   @override
   Widget build(BuildContext context) {
@@ -27,8 +29,8 @@ class FormButton extends StatelessWidget {
             fontWeight: FontWeight.w600,
           ),
           duration: const Duration(milliseconds: 500),
-          child: const Text(
-            "Next",
+          child: Text(
+            text ?? "Next",
             textAlign: TextAlign.center,
           ),
         ),
