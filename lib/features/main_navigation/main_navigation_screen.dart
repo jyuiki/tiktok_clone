0fa00fda41904acd9cdd8d00fb21140b03cac536
daeheonKim <jyuiki@gmail.com>
2023-06-16 00:28:41 +0900

    6.8 Post video Button

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/main_navigation/sff_widget.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
+import 'package:tiktok_clone/features/main_navigation/widgets/post_video_button.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -18,6 +20,19 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
     setState(() {
       _selectedIndex = index;
     });
+  }
+
+  void _onPostVideoButtonTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => Scaffold(
+          appBar: AppBar(
+            title: const Text("Record video"),
+          ),
+        ),
+        fullscreenDialog: true,
+      ),
+    );
   }
 
   @override
@@ -64,6 +79,11 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
                 selsectedIcon: FontAwesomeIcons.solidCompass,
                 onTap: () => _onTap(1),
               ),
+              Gaps.h24,
+              PostVideoButton(
+                onPostVideoButtonTap: _onPostVideoButtonTap,
+              ),
+              Gaps.h24,
               NavTab(
                 text: "Inbox",
                 isSelected: _selectedIndex == 3,
---
+++ lib/features/main_navigation/widgets/post_video_button.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class PostVideoButton extends StatefulWidget {
+  const PostVideoButton({
+    super.key,
+    required Function onPostVideoButtonTap,
+  }) : _onPostVideoButtonTap = onPostVideoButtonTap;
+
+  final Function _onPostVideoButtonTap;
+
+  @override
+  State<PostVideoButton> createState() => _PostVideoButtonState();
+}
+
+class _PostVideoButtonState extends State<PostVideoButton> {
+  final double _height = 30.0;
+  final double _width = 25.0;
+  final double _insetDistance = 20.0;
+  final double _scale = 1.2;
+
+  bool _isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => widget._onPostVideoButtonTap(),
+      onTapDown: (details) {
+        setState(() {
+          _isPressed = true;
+        });
+      },
+      onTapUp: (details) {
+        setState(() {
+          _isPressed = false;
+        });
+      },
+      child: AnimatedSize(
+        clipBehavior: Clip.none,
+        duration: const Duration(milliseconds: 500),
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Positioned(
+              right: _isPressed ? _insetDistance * _scale : _insetDistance,
+              child: Container(
+                height: _isPressed ? _height * _scale : _height,
+                width: _isPressed ? _width * _scale : _width,
+                padding: const EdgeInsets.symmetric(horizontal: Sizes.size8),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF61D4F0),
+                  borderRadius: BorderRadius.circular(Sizes.size8),
+                ),
+              ),
+            ),
+            Positioned(
+              left: _isPressed ? _insetDistance * _scale : _insetDistance,
+              child: Container(
+                height: _isPressed ? _height * _scale : _height,
+                width: _isPressed ? _width * _scale : _width,
+                padding: const EdgeInsets.symmetric(horizontal: Sizes.size8),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(Sizes.size8),
+                ),
+              ),
+            ),
+            Container(
+              height: _isPressed ? _height * _scale : _height,
+              padding: EdgeInsets.symmetric(
+                  horizontal:
+                      _isPressed ? Sizes.size12 * _scale : Sizes.size12),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(Sizes.size6),
+              ),
+              child: Center(
+                child: FaIcon(
+                  FontAwesomeIcons.plus,
+                  color: Colors.black,
+                  size: _isPressed ? 18 * _scale : 18,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
