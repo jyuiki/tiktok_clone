5a8f5919d8dd88376b57817090e55c2b7ba616f8
daeheonKim <jyuiki@gmail.com>
2023-06-19 21:00:45 +0900

    8.0 showModalButtonSheet

---
+++ lib/features/videos/widgets/video_comments.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class VideoComments extends StatefulWidget {
+  const VideoComments({super.key});
+
+  @override
+  State<VideoComments> createState() => _VideoCommentsState();
+}
+
+class _VideoCommentsState extends State<VideoComments> {
+  void _onClosedPressed() {
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          Sizes.size14,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.grey.shade50,
+        appBar: AppBar(
+          backgroundColor: Colors.grey.shade50,
+          automaticallyImplyLeading: false,
+          title: const Text(
+            "222 comments",
+          ),
+          actions: [
+            IconButton(
+              onPressed: _onClosedPressed,
+              icon: const FaIcon(
+                FontAwesomeIcons.xmark,
+              ),
+            ),
+          ],
+        ),
+        body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) => Container(
+            child: const Text("Comment"),
+          ),
+        ),
+      ),
+    );
+  }
+}
