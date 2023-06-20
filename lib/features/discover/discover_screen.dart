28520e991e56c2ecfd5614e40ad81bde9d7526a3
daeheonKim <jyuiki@gmail.com>
2023-06-21 00:20:50 +0900

    9.5.1 Code Challenge

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
@@ -22,9 +21,11 @@ class DiscoverScreen extends StatefulWidget {
   State<DiscoverScreen> createState() => _DiscoverScreenState();
 }
 
-class _DiscoverScreenState extends State<DiscoverScreen> {
-  final TextEditingController _textEditingController =
-      TextEditingController(text: "Initial Text");
+class _DiscoverScreenState extends State<DiscoverScreen>
+    with SingleTickerProviderStateMixin {
+  final TextEditingController _textEditingController = TextEditingController();
+  late final TabController _tabController;
+
   void _onSearchChanged(String value) {
     logger.d(value);
   }
@@ -34,127 +35,169 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      length: tabs.length,
+      vsync: this,
+    )..addListener(() {
+        logger.i("tab changed index = ${tabs[_tabController.index]}");
+        FocusScope.of(context).unfocus();
+      });
+  }
+
+  @override
   void dispose() {
     _textEditingController.dispose();
+    _tabController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: tabs.length,
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          elevation: 1,
-          title: CupertinoSearchTextField(
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        elevation: 1,
+        title: SizedBox(
+          height: Sizes.size44,
+          child: TextField(
             controller: _textEditingController,
             onChanged: _onSearchChanged,
             onSubmitted: _onSearchSubmitted,
-          ),
-          bottom: TabBar(
-            splashFactory: NoSplash.splashFactory,
-            padding: const EdgeInsets.symmetric(horizontal: Sizes.size16),
-            isScrollable: true,
-            labelStyle: const TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: Sizes.size16,
-            ),
-            indicatorColor: Colors.black,
-            unselectedLabelColor: Colors.grey.shade500,
-            labelColor: Colors.black,
-            tabs: [
-              for (var tab in tabs)
-                Tab(
-                  text: tab,
+            textInputAction: TextInputAction.done,
+            decoration: InputDecoration(
+              hintText: "Search",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(Sizes.size4),
+                borderSide: BorderSide.none,
+              ),
+              filled: true,
+              fillColor: Colors.grey.shade200,
+              contentPadding: const EdgeInsets.symmetric(
+                horizontal: Sizes.size20,
+              ),
+              prefixIcon: const Icon(
+                FontAwesomeIcons.magnifyingGlass,
+                opticalSize: 3,
+                color: Colors.black,
+                size: Sizes.size20,
+              ),
+              suffixIcon: GestureDetector(
+                onTap: () => _textEditingController.clear(),
+                child: Icon(
+                  FontAwesomeIcons.solidCircleXmark,
+                  color: Colors.grey.shade600,
+                  size: Sizes.size16 + Sizes.size2,
                 ),
-            ],
+              ),
+            ),
           ),
         ),
-        body: TabBarView(
-          children: [
-            GridView.builder(
-              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-              itemCount: 20,
-              padding: const EdgeInsets.all(Sizes.size6),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: Sizes.size10,
-                mainAxisSpacing: Sizes.size10,
-                childAspectRatio: 9 / 20,
+        bottom: TabBar(
+          controller: _tabController,
+          splashFactory: NoSplash.splashFactory,
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size16),
+          isScrollable: true,
+          labelStyle: const TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: Sizes.size16,
+          ),
+          indicatorColor: Colors.black,
+          unselectedLabelColor: Colors.grey.shade500,
+          labelColor: Colors.black,
+          tabs: [
+            for (var tab in tabs)
+              Tab(
+                text: tab,
               ),
-              itemBuilder: (context, index) => Column(
-                children: [
-                  Container(
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(Sizes.size4),
-                    ),
-                    child: AspectRatio(
-                      aspectRatio: 9 / 16,
-                      child: FadeInImage.assetNetwork(
-                          fit: BoxFit.cover,
-                          placeholder: "assets/images/dayeon.jpeg",
-                          image:
-                              "https://images.unsplash.com/photo-1673844969019-c99b0c933e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"),
-                    ),
-                  ),
-                  Gaps.v10,
-                  const Text(
-                    "This is Very long caption for my tiktok blabalalbalablalblablabalalbalablalblablabalalbalablalbla",
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: Sizes.size16 + Sizes.size2,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Gaps.v5,
-                  DefaultTextStyle(
-                    style: TextStyle(
-                      color: Colors.grey.shade600,
-                      fontWeight: FontWeight.w600,
-                    ),
-                    child: Row(
-                      children: [
-                        const CircleAvatar(
-                          radius: 12,
-                          backgroundImage: NetworkImage(
-                              "https://github.githubassets.com/images/modules/profile/achievements/pull-shark-default.png"),
-                        ),
-                        Gaps.h8,
-                        const Expanded(
-                          child: Text(
-                            "https://github.githubassets.com/images/modules/profile/achi",
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        Gaps.h8,
-                        FaIcon(
-                          FontAwesomeIcons.heart,
-                          size: Sizes.size16,
-                          color: Colors.grey.shade600,
-                        ),
-                        Gaps.h2,
-                        const Text(
-                          "2.5M",
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            for (var tab in tabs.skip(1))
-              Center(
-                child: Text(
-                  tab,
-                  style: const TextStyle(fontSize: 28),
-                ),
-              )
           ],
         ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          GridView.builder(
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+            itemCount: 20,
+            padding: const EdgeInsets.all(Sizes.size6),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: Sizes.size10,
+              mainAxisSpacing: Sizes.size10,
+              childAspectRatio: 9 / 20,
+            ),
+            itemBuilder: (context, index) => Column(
+              children: [
+                Container(
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(Sizes.size4),
+                  ),
+                  child: AspectRatio(
+                    aspectRatio: 9 / 16,
+                    child: FadeInImage.assetNetwork(
+                        fit: BoxFit.cover,
+                        placeholder: "assets/images/dayeon.jpeg",
+                        image:
+                            "https://images.unsplash.com/photo-1673844969019-c99b0c933e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"),
+                  ),
+                ),
+                Gaps.v10,
+                const Text(
+                  "This is Very long caption for my tiktok blabalalbalablalblablabalalbalablalblablabalalbalablalbla",
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontSize: Sizes.size16 + Sizes.size2,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Gaps.v5,
+                DefaultTextStyle(
+                  style: TextStyle(
+                    color: Colors.grey.shade600,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  child: Row(
+                    children: [
+                      const CircleAvatar(
+                        radius: 12,
+                        backgroundImage: NetworkImage(
+                            "https://github.githubassets.com/images/modules/profile/achievements/pull-shark-default.png"),
+                      ),
+                      Gaps.h8,
+                      const Expanded(
+                        child: Text(
+                          "https://github.githubassets.com/images/modules/profile/achi",
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      Gaps.h8,
+                      FaIcon(
+                        FontAwesomeIcons.heart,
+                        size: Sizes.size16,
+                        color: Colors.grey.shade600,
+                      ),
+                      Gaps.h2,
+                      const Text(
+                        "2.5M",
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          for (var tab in tabs.skip(1))
+            Center(
+              child: Text(
+                tab,
+                style: const TextStyle(fontSize: 28),
+              ),
+            )
+        ],
       ),
     );
   }
