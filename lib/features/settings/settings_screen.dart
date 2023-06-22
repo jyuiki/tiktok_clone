f582504870e874974e1fe75376ae5dd91db42212
daeheonKim <jyuiki@gmail.com>
2023-06-22 18:31:27 +0900

    13.2 showDateRangePicker

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/main.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -10,8 +11,45 @@ class SettingsScreen extends StatelessWidget {
         title: const Text("Settings"),
       ),
       body: ListView(
-        children: const [
-          AboutListTile(),
+        children: [
+          ListTile(
+            onTap: () async {
+              final date = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+              );
+              logger.i(date);
+
+              final time = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              logger.i(time);
+
+              final booking = await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+                builder: (context, child) {
+                  return Theme(
+                    data: ThemeData(
+                      appBarTheme: const AppBarTheme(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.black,
+                      ),
+                    ),
+                    child: child!,
+                  );
+                },
+              );
+
+              logger.i(booking);
+            },
+            title: const Text("What is your birthday?"),
+          ),
+          const AboutListTile(),
         ],
       ),
     );
