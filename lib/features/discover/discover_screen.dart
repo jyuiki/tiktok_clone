8f4d8fcef4cc8037b1a64c1625ead9be031a49c7
daeheonKim <jyuiki@gmail.com>
2023-06-22 21:23:01 +0900

    14.4 LayoutBuilder

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -169,67 +169,70 @@ class _DiscoverGridviewState extends State<DiscoverGridview>
         mainAxisSpacing: Sizes.size10,
         childAspectRatio: 9 / 20,
       ),
-      itemBuilder: (context, index) => Column(
-        children: [
-          Container(
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(Sizes.size4),
+      itemBuilder: (context, index) => LayoutBuilder(
+        builder: (context, constraints) => Column(
+          children: [
+            Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Sizes.size4),
+              ),
+              child: AspectRatio(
+                aspectRatio: 9 / 16,
+                child: FadeInImage.assetNetwork(
+                    fit: BoxFit.cover,
+                    placeholder: "assets/images/dayeon.jpeg",
+                    image:
+                        "https://images.unsplash.com/photo-1673844969019-c99b0c933e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"),
+              ),
             ),
-            child: AspectRatio(
-              aspectRatio: 9 / 16,
-              child: FadeInImage.assetNetwork(
-                  fit: BoxFit.cover,
-                  placeholder: "assets/images/dayeon.jpeg",
-                  image:
-                      "https://images.unsplash.com/photo-1673844969019-c99b0c933e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"),
+            Gaps.v10,
+            Text(
+              "${constraints.maxWidth} This is Very long caption for my tiktok blabalalbalablalblablabalalbalablalblablabalalbalablalbla",
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                fontSize: Sizes.size16 + Sizes.size2,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          Gaps.v10,
-          const Text(
-            "This is Very long caption for my tiktok blabalalbalablalblablabalalbalablalblablabalalbalablalbla",
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              fontSize: Sizes.size16 + Sizes.size2,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Gaps.v5,
-          DefaultTextStyle(
-            style: TextStyle(
-              color: Colors.grey.shade600,
-              fontWeight: FontWeight.w600,
-            ),
-            child: Row(
-              children: [
-                const CircleAvatar(
-                  radius: 12,
-                  backgroundImage: NetworkImage(
-                      "https://github.githubassets.com/images/modules/profile/achievements/pull-shark-default.png"),
-                ),
-                Gaps.h8,
-                const Expanded(
-                  child: Text(
-                    "https://github.githubassets.com/images/modules/profile/achi",
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                Gaps.h8,
-                FaIcon(
-                  FontAwesomeIcons.heart,
-                  size: Sizes.size16,
+            Gaps.v5,
+            if (constraints.maxWidth < 200 || constraints.maxWidth > 250)
+              DefaultTextStyle(
+                style: TextStyle(
                   color: Colors.grey.shade600,
+                  fontWeight: FontWeight.w600,
                 ),
-                Gaps.h2,
-                const Text(
-                  "2.5M",
+                child: Row(
+                  children: [
+                    const CircleAvatar(
+                      radius: 12,
+                      backgroundImage: NetworkImage(
+                          "https://github.githubassets.com/images/modules/profile/achievements/pull-shark-default.png"),
+                    ),
+                    Gaps.h8,
+                    const Expanded(
+                      child: Text(
+                        "https://github.githubassets.com/images/modules/profile/achi",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Gaps.h8,
+                    FaIcon(
+                      FontAwesomeIcons.heart,
+                      size: Sizes.size16,
+                      color: Colors.grey.shade600,
+                    ),
+                    Gaps.h2,
+                    const Text(
+                      "2.5M",
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-        ],
+              ),
+          ],
+        ),
       ),
     );
   }
