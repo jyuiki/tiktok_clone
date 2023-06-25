de4b99caa87548b79139f9ddc73085e62f81a5fb
daeheonKim <jyuiki@gmail.com>
2023-06-26 00:19:44 +0900

    18.2 Parameters

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -62,7 +62,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
           ),
           Offstage(
             offstage: _selectedIndex != 4,
-            child: const UserProfileScreen(),
+            child: const UserProfileScreen(username: "FIFA"),
           ),
         ],
       ),
