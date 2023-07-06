d5a96b73f282d36286453bf6a3182d9b5ca3809e
daeheonKim <jyuiki@gmail.com>
2023-07-06 22:22:19 +0900

    27.2 Infinite Firebase

--- lib/features/videos/repos/videos_repo.dart
+++ lib/features/videos/repos/videos_repo.dart
@@ -21,14 +21,20 @@ class VideosRepository {
     await _db.collection("videos").add(data.toJson());
   }
 
-  Future<QuerySnapshot<Map<String, dynamic>>> fetchVideos() {
-    return _db
+  Future<QuerySnapshot<Map<String, dynamic>>> fetchVideos(
+      {int? lastItemCreatedAt}) {
+    final query = _db
         .collection("videos")
         .orderBy(
           "createdAt",
           descending: true,
         )
-        .get();
+        .limit(2);
+    if (lastItemCreatedAt == null) {
+      return query.get();
+    } else {
+      return query.startAfter([lastItemCreatedAt]).get();
+    }
   }
 }
 
